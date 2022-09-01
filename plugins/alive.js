467c55b012bd12aa264f9347497d8c6ec5f595ed
Jaisaljosy1234 <110614735+Jaisaljosy1234@users.noreply.github.com>
2022-09-01 12:24:31 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,14 +1,14 @@
-const { bot, aliveMessage } = require('../lib/')
+const { bot, hiMessage } = require('../lib/')
 
 bot(
 	{
 		pattern: 'hi ?(.*)',
 		fromMe: true,
-		desc: 'bot alive message',
+		desc: 'bot hi message',
 		type: 'misc',
 	},
 	async (message, match) => {
-		const { msg, options, type } = await aliveMessage(match, message)
+		const { msg, options, type } = await hiMessage(match, message)
 		return await message.send(msg, options, type)
 	}
 )
